3546537c9cbf590e46e43ea0d3fc23aa86570b2e
CerberusGH97 <alexstuck7@gmail.com>
2022-12-21 02:49:33 +0000

    Saved progress on IfStatement 2022-12-21 02:49:33 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -17,5 +17,11 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-       if 
+       if (bool==true) {
+        return x;
+       } else {
+    return y;
+       }
+       }
 }
+
