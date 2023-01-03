bc377b364b9d82b8d1ed93fd7886468a89cab3af
CerberusGH97 <alexstuck7@gmail.com>
2023-01-03 20:58:20 +0000

    Saved progress on UncheckedException 2023-01-03 20:58:20 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,6 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int n = 20/0;
     }
 }
