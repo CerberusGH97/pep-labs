d71c78123b32a7b9fae35400fa54931fb00bd7fa
CerberusGH97 <alexstuck7@gmail.com>
2023-01-16 20:19:11 +0000

    Saved progress on RequestBody 2023-01-16 20:19:11 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -17,7 +19,15 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem1", ctx -> {
-                //implement logic here
+                
+        String jsonString = ctx.body();
+
+    ObjectMapper om = new ObjectMapper();
+    Song p1 = om.readValue(jsonString, Song.class);
+
+    ctx.result(p1.getArtistName());
+
+
         });
 
         /**
@@ -28,7 +38,22 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem2", ctx -> {
-               //implement logic here
+           
+            String jsonString = ctx.body();
+
+            ObjectMapper om = new ObjectMapper();
+            Song p2 = om.readValue(jsonString, Song.class);
+
+            //we need to let the request know we will send back json in the body
+            ctx.contentType("application/json"); 
+
+            p2.setArtistName("Beatles");
+    
+            //utilize jackson convert back the user object to a json string
+            String jsonStringToBeReturned = om.writeValueAsString(p2);
+
+            //return the json string in the response body
+            ctx.result(jsonStringToBeReturned);
         });
 
 
