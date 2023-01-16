4733d110ad0278ebba99f1814eced5c9276c9b7c
CerberusGH97 <alexstuck7@gmail.com>
2023-01-16 19:02:23 +0000

    Saved progress on PathParameters 2023-01-16 19:02:23 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -17,6 +17,7 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             String firstName = ctx.pathParam("first");
+            ctx.result(firstName);
 
         });
 
