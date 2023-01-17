f7bab619786310528c52c28e24f45df682711548
CerberusGH97 <alexstuck7@gmail.com>
2023-01-17 01:10:35 +0000

    Saved progress on GetMax 2023-01-17 01:10:35 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -1,3 +1,4 @@
+import org.eclipse.jetty.util.Index;
 
 //
 
@@ -11,6 +12,11 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        return -1;
+        boolean testvalue = true;
+        while(testvalue){
+            for
+        }
+        
+        
     }
 }
