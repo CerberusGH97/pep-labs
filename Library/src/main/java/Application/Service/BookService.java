efad3ac74c7975472788c3f3773726979d99b313
CerberusGH97 <alexstuck7@gmail.com>
2023-01-24 14:30:46 +0000

    Saved progress on Library 2023-01-24 14:30:46 +0000

--- Library/src/main/java/Application/Service/BookService.java
+++ Library/src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return BookDAO.getAllBooks();
+        return bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +51,20 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
+        Book bookfromDB = this.bookDAO.getBookByIsbn(book.getIsbn());
+        
+        if(bookfromDB == null) return null;
+        
+        bookDAO.insertBook(book);
+        return this.bookDAO.getBookByIsbn(book.getIsbn());
 
-        return null;
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return BookDAO.getBooksWithBookCountOverZero();
+        return bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
